21a3c734997323b3b6b607670981d64c4d6118ab
T. Yasusi <yasusi.1530@gmail.com>
2026-05-19 17:17:40 +0900

    pages mock

---
+++ pages/src/createMonacoEditor.js
@@ -0,0 +1,11 @@
+import * as monaco from 'monaco-editor';
+
+export default function(htmlElement){
+    return monaco.editor.create(htmlElement, {
+        language: 'typescript',
+        theme: 'vs-dark',
+        lineNumbers: 'on',
+        fontSize: 14,
+        automaticLayout: true
+    });
+}
---
+++ pages/src/main.js
@@ -0,0 +1,7 @@
+import createMonacoEditor from './createMonacoEditor.js'
+// import babelTransform from './babelTransform.js'
+
+const editor = createMonacoEditor(document.getElementById('editor'));
+console.log(editor);
+
+console.log("main process START")
